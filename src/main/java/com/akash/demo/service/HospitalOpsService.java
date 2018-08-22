a386e9fe042e90527ebee6ae85ea91b3ddd4c5de
roxta <roxta@Genius>
2018-08-23 00:51:01 +0530

    made changes to service class

--- src/main/java/com/akash/demo/service/HospitalOpsService.java
+++ src/main/java/com/akash/demo/service/HospitalOpsService.java
@@ -3,7 +3,6 @@ package com.akash.demo.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.akash.demo.dao.impl.HospitalDAOImpl;
